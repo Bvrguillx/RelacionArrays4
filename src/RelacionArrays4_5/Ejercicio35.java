a8f3c00dbc63cc435314523dcc9e0c2fb7b61750
Alberto ''Khalifa'' <Alberto ''Khalifa''@172.16.100.45>
2017-01-29 23:08:33 +0100

    Relacion de Arrays 4

---
+++ src/RelacionArrays4_5/Ejercicio35.java
@@ -0,0 +1,25 @@
+package RelacionArrays4_5;
+
+public class Ejercicio35 {
+
+	public static void main(String[] args) {
+		
+		// Escribe una función “Elimina1ElementoArray” a la que le pasas dos parámetros: 
+		// un array de enteros y un valor entero. La función eliminará del array el valor entero 
+		// independientemente de la posición en la que se encuentre. Si el valor se repite, 
+		// se eliminará sólo la primera vez que aparece el valor. Nota: el array habrá que pasarlo 
+		// por referencia; esto es necesario siempre que hay que modificar el tamaño de un array.
+
+	}
+	
+	public static int[] Elimina1ElementoArray( int[]ArrayEnteros, int ValorEntero ){
+		
+		int [] Array = {5,10,15,20,25,30};
+		
+		int Valor = 10;
+		
+		return ArrayEnteros;
+		
+	} 
+
+}
